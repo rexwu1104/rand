03b8d8d963b0f3a17b53a2b910814b990bbc9cb3
rexwu1104 <44058270+rexwu1104@users.noreply.github.com>
2020-06-12 09:32:47 +0800

    Update rand.py

--- rand.py
+++ rand.py
@@ -1,9 +1,45 @@
-from time_py import latency as l #導入上述的延遲函式
+import time
 
-seed_out = int(l() * 10000) #讓它放大一萬倍，較好得到整數
+def l():
+  before = time.time()
+  for i in range(10000):
+    continue
 
-def random(times : int): #亂數函式
-  seed = seed_out #取得上面的變數
-  for i in range(times): #執行times次
-    seed = (seed * (2 ** 127 - 1) // int(l() * 10000)) % 65536 #產生偽亂數
-    yield seed #回傳
+  after = time.time()
+  return after - before
+
+class rand():
+  def __init__(self, modmax=None):
+    self.seed_out = int(l() * 10000)
+    if modmax is not None:
+      self.max = modmax
+    else:
+      self.max = 65536
+
+  def rand(self, times : int):
+    num = []
+    seed = self.seed_out
+    if times > self.max:
+      limit = times // self.max 
+      nums = times % self.max
+    else:
+      limit = 1
+      nums = times
+    for i in range(times):
+      seed = (seed * (2 ** 127 - 1) // int(l() * 10000)) % self.max + 1
+      while True:
+        if seed not in num and limit != 0:
+          num.append(seed)
+          break
+        elif len(num) == self.max and limit != 0:
+          limit -= 1
+          num = []
+        elif limit == 0 and len(num) != nums and seed not in num:
+          num.append(seed)
+          break
+        else:
+          seed = seed * (2 ** int(l() * 1000)) * int(l() * 10000) % self.max + 1
+      yield seed
+  
+  def setup(self, max : int):
+    self.max = max
